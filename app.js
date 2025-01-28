57fd8e08e59a43473d40701c42fa0c00b5c4e88e
Victor Santos <143122299+spacevvars@users.noreply.github.com>
2025-01-27 23:22:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function exibirTextoNaTela(tag, texto) {
     if ('speechSynthesis' in window) {
         let utterance = new SpeechSynthesisUtterance(texto);
         utterance.lang = 'pt-BR'; 
-        utterance.rate = 1.5; 
+        utterance.rate = 1.9; 
         window.speechSynthesis.speak(utterance); 
     } else {
         console.log("Web Speech API não suportada neste navegador.");
